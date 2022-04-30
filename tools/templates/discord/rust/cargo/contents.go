042dfc7bada0c6c9119162ef2fb9a76b3d51af3b
abdfnx <abdfnabd@gmail.com>
2022-04-30 10:45:27 +0300

    templates: start building `discord-rust-cargo` template content

---
+++ tools/templates/discord/rust/cargo/contents.go
@@ -0,0 +1,29 @@
+package cargo
+
+import "fmt"
+
+func DockerfileContent(botName string) string {
+	return fmt.Sprintf(`FROM alpine:latest
+FROM rust:alpine
+FROM botwayorg/botway:latest
+
+ENV DISCORD_BOT_NAME="%s"
+ARG DISCORD_TOKEN
+ARG DISCORD_CLIENT_ID
+
+COPY . .
+
+RUN apk update && \
+	apk add --no-cache --virtual build-dependencies build-base gcc git libsodium ffmpeg opus autoconf automake libtool m4 youtube-dl
+
+# Add packages you want
+# RUN apk add PACKAGE_NAME
+
+RUN botway init --docker --name ${DISCORD_BOT_NAME}
+RUN cargo build --release
+RUN cp ./target/release/${DISCORD_BOT_NAME} .
+
+EXPOSE 8000
+
+ENTRYPOINT ["./${DISCORD_BOT_NAME}"]`, botName)
+}
