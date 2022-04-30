24c193c0223953771153456edfe53c1bc800502a
David Tomson <elexhubinfo@gmail.com>
2022-04-30 21:51:29 +0000

    templates: finish upgrading `telegram-rust-*` templates contents

--- tools/templates/telegram/rust/shared.go
+++ tools/templates/telegram/rust/shared.go
@@ -1,53 +1,13 @@
 package rust
 
-import "fmt"
+import "github.com/abdfnx/botway/tools/templates"
 
 func MainRsContent() string {
-	return `use futures::StreamExt;
-use telegram_bot::*;
-
-use botway_rs::get_token;
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-	let api = Api::new(get_token());
-
-	// Fetch new updates via long poll method
-	let mut stream = api.stream();
-
-	while let Some(update) = stream.next().await {
-		// If the received update contains a new message...
-		let update = update?;
-
-		if let UpdateKind::Message(message) = update.kind {
-			if let MessageKind::Text { ref data, .. } = message.kind {
-				// Print received text message to stdout.
-				println!("<{}>: {}", &message.from.first_name, data);
-
-				// Answer message with "Hi".
-				api.send(message.text_reply(format!(
-					"Hi, {}! You just wrote '{}'",
-					&message.from.first_name, data
-				)))
-				.await?;
-			}
-		}
-	}
-
-	Ok(())
-}`
+	return templates.Content("telegram", "rust", "src/main.rs", "")
 }
 
 func CargoFileContent(botName string) string {
-	return fmt.Sprintf(`[package]
-name = "%s"
-version = "0.1.0"
-edition = "2021"
-
-[dependencies]
-telegram-bot = "0.7"
-futures = "0.3.21"
-botway-rs = "0.0.1"`, botName)
+	return templates.Content("telegram", "rust", "Cargo.toml", botName)
 }
 
 func Resources() string {
