dbc987b4100fd2924da7c29b38f9007cc52af7cd
abdfnx <abdfnabd@gmail.com>
2022-04-30 10:38:09 +0300

    templates: `discord-python-pipenv` template builder is finished

---
+++ tools/templates/discord/python/pipenv/index.go
@@ -0,0 +1,68 @@
+package pipenv
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/abdfnx/botway/tools/templates/discord"
+	"github.com/abdfnx/botway/tools/templates/discord/python/pip"
+	"github.com/abdfnx/looker"
+)
+
+func DiscordPythonPipenv(botName string) {
+	pipenv, err := looker.LookPath("pipenv")
+
+	if err != nil {
+		log.Fatalf("error: %s is not installed", pipenv)
+	} else {
+		if runtime.GOOS == "linux" {
+			fmt.Println("Installing some required linux packages")
+
+			discord.InstallCommandPython()
+		}
+
+		resourcesFile := os.WriteFile(filepath.Join(botName, "resources.md"), []byte(pip.Resources()), 0644)
+
+		if resourcesFile != nil {
+			log.Fatal(resourcesFile)
+		}
+
+		pipenvInstall := pipenv + " install discord.py botway.py pynacl"
+
+		cmd := exec.Command("bash", "-c", pipenvInstall)
+
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command("powershell.exe", pipenvInstall)
+		}
+
+		cmd.Dir = botName
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+
+		if err != nil {
+			log.Printf("error: %v\n", err)
+		}
+
+		mainFile := os.WriteFile(filepath.Join(botName, "src", "main.py"), []byte(pip.MainPyContent()), 0644)
+		dockerFile := os.WriteFile(filepath.Join(botName, "Dockerfile"), []byte(DockerfileContent(botName)), 0644)
+		procFile := os.WriteFile(filepath.Join(botName, "Procfile"), []byte("process: pipenv run python3 ./src/main.py"), 0644)
+
+		if mainFile != nil {
+			log.Fatal(mainFile)
+		}
+
+		if dockerFile != nil {
+			log.Fatal(dockerFile)
+		}
+
+		if procFile != nil {
+			log.Fatal(procFile)
+		}
+	}
+}
