e7148e18e7415ff6e80ab9b15bb6c7db56445e6c
abdfnx <abdfnabd@gmail.com>
2022-04-30 11:22:01 +0300

    build the discord handler, add `botway new` main file

---
+++ internal/pipes/new/discord_handler.go
@@ -0,0 +1,38 @@
+package new
+
+import (
+	"github.com/abdfnx/botway/tools/templates/discord/go"
+	"github.com/abdfnx/botway/tools/templates/discord/deno"
+	"github.com/abdfnx/botway/tools/templates/discord/nodejs/npm"
+	"github.com/abdfnx/botway/tools/templates/discord/nodejs/pnpm"
+	"github.com/abdfnx/botway/tools/templates/discord/nodejs/yarn"
+	"github.com/abdfnx/botway/tools/templates/discord/python/pip"
+	"github.com/abdfnx/botway/tools/templates/discord/python/pipenv"
+	"github.com/abdfnx/botway/tools/templates/discord/ruby"
+	"github.com/abdfnx/botway/tools/templates/discord/rust/cargo"
+	"github.com/abdfnx/botway/tools/templates/discord/rust/fleet"
+)
+
+func DiscordHandler(m model) {
+	if m.PlatformChoice == 0 && m.LangChoice == 0 && m.PMCoice == 0 {
+		pip.DiscordPythonPip(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 0 && m.PMCoice == 1 {
+		pipenv.DiscordPythonPipenv(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 1 {
+		dgo.DiscordGo(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 2 && m.PMCoice == 0 {
+		npm.DiscordNodejsNpm(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 2 && m.PMCoice == 1 {
+		yarn.DiscordNodejsYarn(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 2 && m.PMCoice == 2 {
+		pnpm.DiscordNodejsPnpm(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 3 {
+		ruby.DiscordRuby(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 4 && m.PMCoice == 0 {
+		cargo.DiscordRustCargo(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 4 && m.PMCoice == 1 {
+		fleet.DiscordRustFleet(opts.BotName)
+	} else if m.PlatformChoice == 0 && m.LangChoice == 5 {
+		deno.DiscordDeno(opts.BotName)
+	}
+}
---
+++ internal/pipes/new/new.go
@@ -0,0 +1,70 @@
+package new
+
+import (
+	"fmt"
+
+	"github.com/abdfnx/botway/internal/options"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/muesli/reflow/indent"
+)
+
+func New(o *options.NewOptions) {
+	m := model{0, false, 10, 0, 0, false, false, 0, false, 0, false}
+	p := tea.NewProgram(m)
+
+	opts.BotName = o.BotName
+
+	if err := p.Start(); err != nil {
+		fmt.Println("could not start program:", err)
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	if msg, ok := msg.(tea.KeyMsg); ok {
+		k := msg.String()
+
+		if k == "q" || k == "esc" || k == "ctrl+c" {
+			m.Quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	if !m.Platform {
+		return updatePlatforms(msg, m)
+	}
+
+	if !m.Lang {
+		return updateLangs(msg, m)
+	}
+
+	if !m.PM {
+		return updatePMs(msg, m)
+	}
+
+	return buildBot(msg, m, opts.BotName)
+}
+
+func (m model) View() string {
+	var s string
+
+	if m.Quitting {
+		return finalView(m)
+	}
+
+	if !m.Platform {
+		s = platformsView(m)
+		return indent.String("\n"+s+"\n\n", 2)
+	} else if !m.Lang {
+		s = langsView(m)
+		return indent.String("\n"+s+"\n\n", 2)
+	} else if !m.PM {
+		s = pmsView(m)
+		return indent.String("\n"+s+"\n\n", 2)
+	}
+
+	return s
+}
