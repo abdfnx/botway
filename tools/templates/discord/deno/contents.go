cf777e31bc409abe6119f6afc5b6054a08403922
David Tomson <elexhubinfo@gmail.com>
2022-04-30 20:18:17 +0000

    templates: upgrade `discord-deno` template contents

--- tools/templates/discord/deno/contents.go
+++ tools/templates/discord/deno/contents.go
@@ -1,49 +1,75 @@
 package deno
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/abdfnx/resto/core/api"
+)
+
+func Content(fileName, botName string) string {
+	url := "https://raw.githubusercontent.com/abdfnx/botway/main/tools/templates/discord/deno/assets/" + fileName
+	respone, status, _, err := api.BasicGet(url, "GET", "", "", "", "", false, 0, nil)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if status == "404" || status == "401" || strings.Contains(respone, "404") {
+		fmt.Println("404")
+		os.Exit(0)
+	}
+
+	if fileName == "Dockerfile" {
+		return strings.ReplaceAll(respone, "{{.Discord_Bot_name}}", botName)
+	} else {
+		return respone
+	}
+}
 
 func DockerfileContent(botName string) string {
-	return ""
+	return Content("Dockerfile", botName)
 }
 
 func ModTsContent() string {
-	return ""
+	return Content("mod.ts", "")
 }
 
 func DepsTsContent() string {
-	return ""
+	return Content("deps.ts", "")
 }
 
 func CommandsModTsContent() string {
-	return ""
+	return Content("src/commands/mod.ts", "")
 }
 
 func CommandsPingTsContent() string {
-	return ""
+	return Content("src/commands/ping.ts", "")
 }
 
 func EventsGuildCreateTsContent() string {
-	return ""
+	return Content("src/events/guildCreate.ts", "")
 }
 
 func EventsInteractionCreateTsContent() string {
-	return ""
+	return Content("src/events/interactionCreate.ts", "")
 }
 
 func EventsModTsContent() string {
-	return ""
+	return Content("src/events/mod.ts", "")
 }
 
 func EventsReadyTsContent() string {
-	return ""
+	return Content("src/events/ready.ts", "")
 }
 
 func UtilsHelpersTsContent() string {
-	return ""
+	return Content("src/utils/helpers.ts", "")
 }
 
 func UtilsLoggerTsContent() string {
-	return ""
+	return Content("src/utils/logger.ts", "")
 }
 
 func Resources() string {
