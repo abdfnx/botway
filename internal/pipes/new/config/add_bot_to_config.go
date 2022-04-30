1730293f09e8e70d9091f51bba41d1cd561ff751
abdfnx <abdfnabd@gmail.com>
2022-04-30 11:18:59 +0300

    create adding bot to botway config handler in `internal/pipes/new/config`

---
+++ internal/pipes/new/config/add_bot_to_config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/abdfnx/botway/tools"
+	"github.com/abdfnx/tran/dfs"
+	"github.com/gookit/config/v2"
+	cyaml "github.com/gookit/config/v2/yaml"
+)
+
+func AddBotToConfig(botName, botType, botPath string) {
+	homeDir, _ := dfs.GetHomeDirectory()
+	botwayConfigPath := filepath.Join(homeDir, ".botway", "botway.yaml")
+	botwayConfig, err := ioutil.ReadFile(botwayConfigPath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	bc := config.New(".")
+	bc.AddDriver(cyaml.Driver)
+	bcp := bc.LoadSources(config.Yaml, botwayConfig)
+
+	if bcp != nil {
+		panic(bcp)
+	}
+
+	bc.Set("botway.bots." + botName + ".type", botType)
+	bc.Set("botway.bots." + botName + ".path", botPath)
+
+	remove := os.Remove(botwayConfigPath)
+
+	if remove != nil {
+        log.Fatal(remove)
+    }
+
+	newBotConfig := os.WriteFile(botwayConfigPath, []byte(string(tools.ToYaml(bc.Data()))), 0644)
+
+	if newBotConfig != nil {
+		panic(newBotConfig)
+	}
+
+	tools.YamlFMT(botwayConfigPath)
+}
