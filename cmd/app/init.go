2a3973b5aef922170ad35c978f83acf7cc1a6a95
abdfnx <abdfnabd@gmail.com>
2022-04-30 11:24:06 +0300

    start building `botway` cobra commands in `cmd/app` module

---
+++ cmd/app/init.go
@@ -0,0 +1,30 @@
+package app
+
+import (
+	"github.com/abdfnx/botway/internal/pipes/initx"
+	"github.com/abdfnx/botway/internal/options"
+	"github.com/spf13/cobra"
+)
+
+var opts = options.InitOptions{
+	Docker: false,
+}
+
+func InitCMD() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "init",
+		Short: "Initialize ~/.botway .",
+		Aliases: []string{"."},
+		Run: func(cmd *cobra.Command, args []string) {
+			if opts.Docker {
+				initx.DockerInit()
+			} else {
+				initx.BotwayInit()
+			}
+		},
+	}
+
+	cmd.Flags().BoolVarP(&opts.Docker, "docker", "", false, "Initialize botway config in docker")
+
+	return cmd
+}
