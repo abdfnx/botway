4841736f1de8978fa091321679d65c9460152fe4
David Tomson <elexhubinfo@gmail.com>
2022-04-30 20:22:16 +0000

    templates: upgrade `discord-go` template contents and builder

---
+++ tools/templates/discord/go/assets/src/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/abdfnx/botwaygo"
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	dg, err := discordgo.New("Bot " + botwaygo.GetToken())
+
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.\nPress CTRL-C to exit.")
+
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// In this example, we only care about messages that are "ping".
+	if m.Content != "ping" {
+		return
+	}
+
+	// We create the private channel with the user who sent the message.
+	channel, err := s.UserChannelCreate(m.Author.ID)
+
+	if err != nil {
+		// If an error occurred, we failed to create the channel.
+		//
+		// Some common causes are:
+		// 1. We don't share a server with the user (not possible here).
+		// 2. We opened enough DM channels quickly enough for Discord to
+		//    label us as abusing the endpoint, blocking us from opening
+		//    new ones.
+		fmt.Println("error creating channel:", err)
+
+		s.ChannelMessageSend(
+			m.ChannelID,
+			"Something went wrong while sending the DM!",
+		)
+
+		return
+	}
+
+	// Then we send the message through the channel we created.
+	_, err = s.ChannelMessageSend(channel.ID, "Pong!")
+
+	if err != nil {
+		// If an error occurred, we failed to send the message.
+		//
+		// It may occur either when we do not share a server with the
+		// user (highly unlikely as we just received a message) or
+		// the user disabled DM in their settings (more likely).
+		fmt.Println("error sending DM message:", err)
+		s.ChannelMessageSend(
+			m.ChannelID,
+			"Failed to send you a DM. "+
+				"Did you disable DM in your privacy settings?",
+		)
+	}
+}
--- tools/templates/discord/go/contents.go
+++ tools/templates/discord/go/contents.go
@@ -1,133 +1,39 @@
 package dgo
 
-import "fmt"
-
-func DockerfileContent(botName string) string {
-	return fmt.Sprintf(`FROM alpine:latest
-FROM golang:alpine AS builder
-FROM botwayorg/botway:latest
-
-ENV DISCORD_BOT_NAME="%s"
-ARG DISCORD_TOKEN
-ARG DISCORD_CLIENT_ID
-
-COPY . .
-
-RUN apk update && \
-	apk add --no-cache --virtual build-dependencies build-base gcc git
-
-# Add packages you want
-# RUN apk add PACKAGE_NAME
-
-RUN botway init --docker --name ${DISCORD_BOT_NAME}
-RUN go mod tidy
-RUN go build src/main.go -o ${DISCORD_BOT_NAME}
-
-COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
-
-EXPOSE 8000
-
-ENTRYPOINT ["./${DISCORD_BOT_NAME}"]`, botName)
-}
-
-func MainGoContent() string {
-	return `package main
-
 import (
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
+	"strings"
 
-	"github.com/abdfnx/botwaygo"
-	"github.com/bwmarrin/discordgo"
+	"github.com/abdfnx/resto/core/api"
 )
 
-func main() {
-	dg, err := discordgo.New("Bot " + botwaygo.GetToken())
+func Content(fileName, botName string) string {
+	url := "https://raw.githubusercontent.com/abdfnx/botway/main/tools/templates/discord/go/assets/" + fileName
+	respone, status, _, err := api.BasicGet(url, "GET", "", "", "", "", false, 0, nil)
 
 	if err != nil {
-		fmt.Println("error creating Discord session,", err)
-		return
+		fmt.Println(err.Error())
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
-
-	// In this example, we only care about receiving message events.
-	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
-	// Open a websocket connection to Discord and begin listening.
-	err = dg.Open()
-
-	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
+	if status == "404" || status == "401" || strings.Contains(respone, "404") {
+		fmt.Println("404")
+		os.Exit(0)
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.\nPress CTRL-C to exit.")
-
-	sc := make(chan os.Signal, 1)
-
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-
-	<-sc
-
-	// Cleanly close down the Discord session.
-	dg.Close()
+	if fileName == "Dockerfile" {
+		return strings.ReplaceAll(respone, "{{.Discord_Bot_name}}", botName)
+	} else {
+		return respone
+	}
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
+func DockerfileContent(botName string) string {
+	return Content("Dockerfile", botName)
+}
 
-	// In this example, we only care about messages that are "ping".
-	if m.Content != "ping" {
-		return
-	}
-
-	// We create the private channel with the user who sent the message.
-	channel, err := s.UserChannelCreate(m.Author.ID)
-
-	if err != nil {
-		// If an error occurred, we failed to create the channel.
-		//
-		// Some common causes are:
-		// 1. We don't share a server with the user (not possible here).
-		// 2. We opened enough DM channels quickly enough for Discord to
-		//    label us as abusing the endpoint, blocking us from opening
-		//    new ones.
-		fmt.Println("error creating channel:", err)
-
-		s.ChannelMessageSend(
-			m.ChannelID,
-			"Something went wrong while sending the DM!",
-		)
-
-		return
-	}
-
-	// Then we send the message through the channel we created.
-	_, err = s.ChannelMessageSend(channel.ID, "Pong!")
-
-	if err != nil {
-		// If an error occurred, we failed to send the message.
-		//
-		// It may occur either when we do not share a server with the
-		// user (highly unlikely as we just received a message) or
-		// the user disabled DM in their settings (more likely).
-		fmt.Println("error sending DM message:", err)
-		s.ChannelMessageSend(
-			m.ChannelID,
-			"Failed to send you a DM. "+
-				"Did you disable DM in your privacy settings?",
-		)
-	}
-}`
+func MainGoContent() string {
+	return Content("src/main.go", "")
 }
 
 func Resources() string {
