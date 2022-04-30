322fab8647d0f2f6149bebb8812964eb647fc450
abdfnx <abdfnabd@gmail.com>
2022-04-30 10:58:09 +0300

    start building `init` pipe in `internal/pipes` package, tidy modules

--- go.mod
+++ go.mod
@@ -6,7 +6,10 @@ require (
 	github.com/AlecAivazis/survey/v2 v2.3.4
 	github.com/MakeNowJust/heredoc v1.0.0
 	github.com/abdfnx/looker v0.1.0
+	github.com/abdfnx/tran v0.1.31
 	github.com/briandowns/spinner v1.18.1
+	github.com/charmbracelet/bubbles v0.10.3
+	github.com/charmbracelet/bubbletea v0.20.0
 	github.com/charmbracelet/lipgloss v0.5.0
 	github.com/elewis787/boa v0.0.2
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
@@ -15,24 +18,35 @@ require (
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d
 	github.com/muesli/termenv v0.11.1-0.20220212125758-44cd13922739
 	github.com/spf13/cobra v1.4.0
+	github.com/spf13/viper v1.11.0
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
-	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
-	github.com/charmbracelet/bubbles v0.10.3 // indirect
-	github.com/charmbracelet/bubbletea v0.20.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
-	github.com/fatih/color v1.7.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/sahilm/fuzzy v0.1.0 // indirect
+	github.com/spf13/afero v1.8.2 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ internal/pipes/initx/init.go
@@ -0,0 +1,218 @@
+package initx
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/abdfnx/botway/constants"
+	"github.com/abdfnx/tran/dfs"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/viper"
+)
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(constants.PRIMARY_COLOR)
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle.Copy()
+	noStyle             = lipgloss.NewStyle()
+
+	focusedButton = focusedStyle.Copy().Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+func (m model) InitCmd() {
+	var err error
+
+	homeDir, err := dfs.GetHomeDirectory()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = dfs.CreateDirectory(filepath.Join(homeDir, ".botway"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	botwayDirPath := ""
+
+	if runtime.GOOS == "windows" {
+		botwayDirPath = `$HOME\\.botway`
+	} else {
+		botwayDirPath = `$HOME/.botway`
+	}
+
+	viper.AddConfigPath(botwayDirPath)
+	viper.SetConfigName("botway")
+	viper.SetConfigType("yaml")
+
+	viper.SetDefault("botway.bots", "")
+	viper.SetDefault("user.github_username", m.inputs[0].Value())
+	viper.SetDefault("user.docker_id", m.inputs[1].Value())
+
+	if err := viper.SafeWriteConfig(); err != nil {
+		if os.IsNotExist(err) {
+			err = viper.WriteConfig()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatal(err)
+		}
+	}
+
+	botwayConfigFile := filepath.Join(homeDir, ".botway", "botway.yaml")
+
+	if _, err := os.Stat(botwayConfigFile); err == nil {
+		fmt.Print(constants.SUCCESS_BACKGROUND.Render("SUCCESS"))
+		fmt.Println(constants.SUCCESS_FOREGROUND.Render(" Initialization Successful"))
+	} else if errors.Is(err, os.ErrNotExist) {
+		fmt.Print(constants.FAIL_BACKGROUND.Render("ERROR"))
+		fmt.Println(constants.FAIL_FOREGROUND.Render(" Initialization Failed, try again"))
+	}
+}
+
+type model struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode textinput.CursorMode
+}
+
+func initialModel() model {
+	m := model{
+		inputs: make([]textinput.Model, 2),
+	}
+
+	var t textinput.Model
+
+	for i := range m.inputs {
+		t = textinput.New()
+		t.CursorStyle = cursorStyle
+		t.CharLimit = 32
+
+		switch i {
+			case 0:
+				t.Placeholder = "GitHub Username"
+				t.Focus()
+				t.PromptStyle = focusedStyle
+				t.TextStyle = focusedStyle
+
+			case 1:
+				t.Placeholder = "Docker Hub ID"
+				t.CharLimit = 64
+		}
+
+		m.inputs[i] = t
+	}
+
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.String() {
+				case "ctrl+c", "esc":
+					return m, tea.Quit
+
+				case "tab", "shift+tab", "enter", "up", "down":
+					s := msg.String()
+
+					if s == "enter" && m.focusIndex == len(m.inputs) {
+						m.InitCmd()
+
+						return m, tea.Quit
+					}
+
+					if s == "up" || s == "shift+tab" {
+						m.focusIndex--
+					} else {
+						m.focusIndex++
+					}
+
+					if m.focusIndex > len(m.inputs) {
+						m.focusIndex = 0
+					} else if m.focusIndex < 0 {
+						m.focusIndex = len(m.inputs)
+					}
+
+					cmds := make([]tea.Cmd, len(m.inputs))
+
+					for i := 0; i <= len(m.inputs)-1; i++ {
+						if i == m.focusIndex {
+							cmds[i] = m.inputs[i].Focus()
+							m.inputs[i].PromptStyle = focusedStyle
+							m.inputs[i].TextStyle = focusedStyle
+							continue
+						}
+
+						m.inputs[i].Blur()
+						m.inputs[i].PromptStyle = noStyle
+						m.inputs[i].TextStyle = noStyle
+					}
+
+					return m, tea.Batch(cmds...)
+			}
+	}
+
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	var cmds = make([]tea.Cmd, len(m.inputs))
+
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	b.WriteString("\n")
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	return b.String()
+}
+
+func BotwayInit() {
+	if err := tea.NewProgram(initialModel()).Start(); err != nil {
+		fmt.Printf("could not start program: %s\n", err)
+		os.Exit(1)
+	}
+}
