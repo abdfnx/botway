e633ef70c1bffaf929b9a7e97958377d0f187bd7
David Tomson <elexhubinfo@gmail.com>
2022-05-04 20:32:17 +0000

    fix `botwaygo` package, add `.snyk` and `.gitattributes` files

--- packages/botwaygo/main.go
+++ packages/botwaygo/main.go
@@ -2,20 +2,10 @@ package botwaygo
 
 import (
 	"errors"
-	"io/ioutil"
-	"log"
-	"path/filepath"
-	"runtime"
 
-	"github.com/abdfnx/tran/dfs"
+	"github.com/abdfnx/botway/constants"
 	"github.com/spf13/viper"
 	"github.com/tidwall/gjson"
-)
-
-var (
-	homeDir, err = dfs.GetHomeDirectory()
-	botwayConfigFile = filepath.Join(homeDir, ".botway", "botway.json")
-	botwayConfig, berr = ioutil.ReadFile(botwayConfigFile)
 )
 
 func GetBotInfo(value string) string {
@@ -27,32 +17,22 @@ func GetBotInfo(value string) string {
 }
 
 func GetToken() string {
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if berr != nil {
-		panic(berr)
+	if constants.Berr != nil {
+		panic(constants.Berr)
 	}
 
 	if GetBotInfo("lang") != "go" {
 		panic(errors.New("ERROR: Botway is not running in Golang"))
 	} else {
-		data := gjson.Get(string(botwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_token").String()
+		data := gjson.Get(string(constants.BotwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_token").String()
 
 		return data
 	}
-
-	return ""
 }
 
 func GetAppId() string {
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if berr != nil {
-		panic(berr)
+	if constants.Berr != nil {
+		panic(constants.Berr)
 	}
 
 	if GetBotInfo("lang") != "go" {
@@ -61,24 +41,18 @@ func GetAppId() string {
 		data := ""
 
 		if GetBotInfo("type") == "slack" {
-			data = gjson.Get(string(botwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_app_token").String()
+			data = gjson.Get(string(constants.BotwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_app_token").String()
 		} else {
-			data = gjson.Get(string(botwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_app_id").String()
+			data = gjson.Get(string(constants.BotwayConfig), "botway.bots." + GetBotInfo("name") + ".bot_app_id").String()
 		}
 
 		return data
 	}
-
-	return ""
 }
 
 func GetGuildId(serverName string) string {
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if berr != nil {
-		panic(berr)
+	if constants.Berr != nil {
+		panic(constants.Berr)
 	}
 
 	if GetBotInfo("lang") != "go" {
@@ -86,10 +60,8 @@ func GetGuildId(serverName string) string {
 	} else if GetBotInfo("type") != "discord" {
 		panic(errors.New("ERROR: This function/feature is only working with discord bots."))
 	} else {
-		data := gjson.Get(string(botwayConfig), "botway.bots." + GetBotInfo("name") + ".guilds." + serverName + ".server_id").String()
+		data := gjson.Get(string(constants.BotwayConfig), "botway.bots." + GetBotInfo("name") + ".guilds." + serverName + ".server_id").String()
 
 		return data
 	}
-
-	return ""
 }
