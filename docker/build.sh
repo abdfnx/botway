a32944339dfaccc987a54efdf2126a345abdd68d
Abdfn <64256993+abdfnx@users.noreply.github.com>
2022-05-11 22:25:40 +0300

    update docker ci workflow, fix docker build script

--- docker/build.sh
+++ docker/build.sh
@@ -7,5 +7,5 @@ if [ $tag == "latest" ]; then
     filename="alpine"
 fi
 
-docker build -t botwayorg/botway:$tag --file ./docker/$tag.dockerfile .
+docker build -t botwayorg/botway:$tag --file ./docker/$filename.dockerfile .
 docker push botwayorg/botway:$tag
