08425dd0a20a0514adf79860f24d8b49d439e9e6
abdfnx <abdfnabd@gmail.com>
2022-05-11 19:19:19 +0000

    fix docker building script

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,10 @@
-docker build -t botwayorg/botway:$1 --file ./docker/$1.dockerfile .
-docker push botwayorg/botway:$1
+#!/bin/bash
+
+tag="$1"
+
+if [ $tag == "latest" ]; then
+    tag="alpine"
+fi
+
+docker build -t botwayorg/botway:$tag --file ./docker/$tag.dockerfile .
+docker push botwayorg/botway:$tag
