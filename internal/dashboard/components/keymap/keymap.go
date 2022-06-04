f5fb4ee722eb15ea57d071c431ded1447e09be26
abdfnx <abdfnabd@gmail.com>
2022-06-04 10:19:36 +0300

    create key map of botway dashboard

---
+++ internal/dashboard/components/keymap/keymap.go
@@ -0,0 +1,59 @@
+package keymap
+
+import "github.com/charmbracelet/bubbles/key"
+
+type KeyMap struct {
+	Quit     	  key.Binding
+	Down     	  key.Binding
+	Up       	  key.Binding
+	Left     	  key.Binding
+	Right    	  key.Binding
+	Esc      	  key.Binding
+	SwapView 	  key.Binding
+	OpenAtRailway key.Binding
+	PageDown      key.Binding
+	PageUp        key.Binding
+}
+
+func New() KeyMap {
+	return KeyMap{
+		Quit: key.NewBinding(
+			key.WithKeys("ctrl+q", "ctrl+c"),
+			key.WithHelp("ctrl+q", "quit"),
+		),
+		Down: key.NewBinding(
+			key.WithKeys("down"),
+			key.WithHelp("↓", "move down"),
+		),
+		Up: key.NewBinding(
+			key.WithKeys("up"),
+			key.WithHelp("↑", "move up"),
+		),
+		Left: key.NewBinding(
+			key.WithKeys("left"),
+			key.WithHelp("↓", "move down"),
+		),
+		Right: key.NewBinding(
+			key.WithKeys("right"),
+			key.WithHelp("↑", "move up"),
+		),
+		Esc: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("escape", "Reset"),
+		),
+		SwapView: key.NewBinding(
+			key.WithKeys("tab"),
+			key.WithHelp("tab", "select"),
+		),
+		OpenAtRailway: key.NewBinding(
+			key.WithKeys("ctrl+o"),
+			key.WithHelp("ctrl+o", "Open bot project at Railway"),
+		),
+		PageDown: key.NewBinding(
+			key.WithKeys("pgdown", " ", "f"),
+		),
+		PageUp: key.NewBinding(
+			key.WithKeys("pgup", "b"),
+		),
+	}
+}
