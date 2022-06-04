b4a7f591f214ff1c9cd7c760b4855fa275f0e112
abdfnx <abdfnabd@gmail.com>
2022-06-04 11:37:27 +0300

    remove botIndex from Bubble model, create botway bots handlers

---
+++ internal/dashboard/bots.go
@@ -0,0 +1,31 @@
+package dashboard
+
+import (
+	"github.com/abdfnx/botway/constants"
+	"github.com/tidwall/gjson"
+)
+
+var (
+	bots = gjson.Get(string(constants.BotwayConfig), "botway.bots_names")
+	bots_count = gjson.Get(string(constants.BotwayConfig), "botway.bots_names.#").Int()
+)
+
+func (b Bubble) botInfo(infoToGet string) string {
+	v := ""
+
+	bots.ForEach(func(i, value gjson.Result) bool {
+		if b.bubbles.primaryPaginator.Cursor == int(i.Int()) && !b.nav.listCursorHide {
+			v = value.String()
+		}
+
+		return true
+	})
+
+	bot := gjson.Get(string(constants.BotwayConfig), "botway.bots." + v)
+
+	if infoToGet == "token" {
+		return gjson.Get(bot.String(), "bot_token").String()
+	}
+
+	return gjson.Get(bot.String(), infoToGet).String()
+}
--- internal/dashboard/dash.go
+++ internal/dashboard/dash.go
@@ -11,7 +11,6 @@ import (
 type ErrorMsg error
 
 type Bubble struct {
-	botIndex       int
 	bubbles        Bubbles
 	keyMap         keymap.KeyMap
 	nav            Nav
