2615b50174b75022c66b7570a3cf30595d1f3fd3
abdfnx <abdfnabd@gmail.com>
2022-06-07 13:07:59 +0300

    fix botway cdn assets installers dockerfile, update unix installer

--- installer/unix/install.sh
+++ installer/unix/install.sh
@@ -4,11 +4,11 @@ echo Installing Botway installer...
 
 # Download the installer
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-    wget https://botway.web.app/installer-macos -O installer
+    wget https://cdn-botway.up.railway.app/installers/installer-macos -O installer
 fi
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
-    wget https://botway.web.app/installer-linux -O installer
+    wget https://cdn-botway.up.railway.app/installers/installer-linux -O installer
 fi
 
 # Make it an executable
