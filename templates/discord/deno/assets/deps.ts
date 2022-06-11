7f32f4d178d8db2506c1db37de474f7b5754255d
abdfnx <abdfnabd@gmail.com>
2022-06-11 16:35:38 +0300

    update `dependabot` config, move all templates assets to @botwayorg org

--- templates/discord/deno/assets/deps.ts
+++
@@ -1,4 +0,0 @@
-export * from "https://deno.land/x/discordeno@13.0.0-rc31/mod.ts";
-export * from "https://deno.land/x/discordeno@13.0.0-rc31/plugins/mod.ts";
-export * from "https://deno.land/std/fmt/colors.ts";
-export * from "https://deno.land/x/denobot/mod.ts";
