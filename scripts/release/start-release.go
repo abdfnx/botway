807b2216ab227d03d6f864074c2ed82bf0c08fc8
abdfnx <abdfnabd@gmail.com>
2022-06-25 15:04:39 +0300

    finish from botway release script, bump to `v0.1.3`

---
+++ scripts/release/start-release.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"github.com/tidwall/sjson"
+)
+
+func main() {
+	updateVersionOnPackageJSON(os.Args[1])
+	gitTag(os.Args[1])
+	gitPushOrigin(os.Args[1])
+	publishOnNPM()
+}
+
+func updateVersionOnPackageJSON(version string) {
+	newVersion, err := sjson.Set("package.json", "version", version)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("package.json", []byte(newVersion), 0644)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func gitTag(version string) {
+	cmd := "git tag -a " + version + " -m \"New release " + version + "\""
+
+	runCmd := exec.Command("bash", "-c", cmd)
+
+	if runtime.GOOS == "windows" {
+		runCmd = exec.Command("powershell.exe", cmd)
+	}
+
+	runCmd.Stdin = os.Stdin
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	err := runCmd.Run()
+
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+}
+
+func gitPushOrigin(version string) {
+	cmd := "git push origin " + version
+	runCmd := exec.Command("bash", "-c", cmd)
+
+	if runtime.GOOS == "windows" {
+		runCmd = exec.Command("powershell.exe", cmd)
+	}
+
+	runCmd.Stdin = os.Stdin
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	err := runCmd.Run()
+
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+}
+
+func publishOnNPM() {
+	cmd := "yarn publish"
+
+	runCmd := exec.Command("bash", "-c", cmd)
+
+	if runtime.GOOS == "windows" {
+		runCmd = exec.Command("powershell.exe", cmd)
+	}
+
+	runCmd.Stdin = os.Stdin
+	runCmd.Stdout = os.Stdout
+	runCmd.Stderr = os.Stderr
+	err := runCmd.Run()
+
+	if err != nil {
+		log.Printf("error: %v\n", err)
+	}
+}
