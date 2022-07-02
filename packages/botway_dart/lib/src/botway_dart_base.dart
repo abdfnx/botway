4b4abaf3915d94d39727daae6db593c5c1ebaf1e
Abdfn <64256993+abdfnx@users.noreply.github.com>
2022-07-02 08:49:33 +0000

    format and documenting `botway_dart` library

--- packages/botway_dart/lib/src/botway_dart_base.dart
+++ packages/botway_dart/lib/src/botway_dart_base.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:yaml/yaml.dart';
 import 'package:path/path.dart' as p;
 
+/// This is Botway class
 class Botway {
   get_bot_info(String value) {
     if (File(".botway.yaml").existsSync()) {
@@ -30,10 +31,12 @@ class Botway {
 
   dynamic get BotwayConfig => json.decode(BotwayConfigFile());
 
+  /// This function returns your bot token
   Get_Token() {
     return BotwayConfig["botway"]["bots"][get_bot_info("name")]["bot_token"];
   }
 
+  /// This function returns your bot app id (or app token for `slack`)
   Get_App_Id() {
     String value = "bot_app_id";
 
@@ -44,19 +47,25 @@ class Botway {
     return BotwayConfig["botway"]["bots"][get_bot_info("name")][value];
   }
 
+  /// This function returns your guild id of a server
   Get_Guild_Id(String server_name) {
     if (get_bot_info("type") != "discord") {
-      throw Exception("ERROR: This function/feature is only working with discord bots");
+      throw Exception(
+          "ERROR: This function/feature is only working with discord bots");
     } else {
-      return BotwayConfig["botway"]["bots"][get_bot_info("name")]["guilds"][server_name]["server_id"];
+      return BotwayConfig["botway"]["bots"][get_bot_info("name")]["guilds"]
+          [server_name]["server_id"];
     }
   }
 
+  /// This function returns the signing secret of your slack bot
   Get_Signing_Secret() {
     if (get_bot_info("type") != "slack") {
-      throw Exception("ERROR: This function/feature is only working with slack bots");
+      throw Exception(
+          "ERROR: This function/feature is only working with slack bots");
     } else {
-      return BotwayConfig["botway"]["bots"][get_bot_info("name")]["signing_secret"];
+      return BotwayConfig["botway"]["bots"][get_bot_info("name")]
+          ["signing_secret"];
     }
   }
 }
