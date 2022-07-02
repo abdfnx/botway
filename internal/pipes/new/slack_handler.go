8cb24347e96f688bbd9c5c2a5e7d00f7a60eb8d8
abdfnx <abdfnabd@gmail.com>
2022-07-02 23:20:41 +0300

    finish from supporting `poetry` for all botway python templates #50

--- internal/pipes/new/slack_handler.go
+++ internal/pipes/new/slack_handler.go
@@ -4,6 +4,7 @@ import (
 	"github.com/abdfnx/botway/templates/slack/nodejs"
 	"github.com/abdfnx/botway/templates/slack/python/pip"
 	"github.com/abdfnx/botway/templates/slack/python/pipenv"
+	"github.com/abdfnx/botway/templates/slack/python/poetry"
 )
 
 func SlackHandler(m model) {
@@ -11,6 +12,8 @@ func SlackHandler(m model) {
 		pip.SlackPythonPip(opts.BotName)
 	} else if m.PlatformChoice == 2 && m.LangChoice == 0 && m.PMCoice == 1 {
 		pipenv.SlackPythonPipenv(opts.BotName)
+	} else if m.PlatformChoice == 2 && m.LangChoice == 0 && m.PMCoice == 2 {
+		poetry.SlackPythonPoetry(opts.BotName)
 	} else if m.PlatformChoice == 2 && m.LangChoice == 1 && m.PMCoice == 0 {
 		nodejs.SlackNodejs(opts.BotName, "npm")
 	} else if m.PlatformChoice == 2 && m.LangChoice == 1 && m.PMCoice == 1 {
---
+++ templates/slack/python/poetry/index.go
@@ -0,0 +1,81 @@
+package poetry
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/abdfnx/botway/constants"
+	"github.com/abdfnx/botway/templates"
+	"github.com/abdfnx/botway/templates/slack/python"
+	"github.com/abdfnx/looker"
+)
+
+func PyProjectContent(botName string) string {
+	return templates.Content("pyproject.toml", "discord-python", botName)
+}
+
+func SlackPythonPoetry(botName string) {
+	pythonPath := "python3"
+
+	if runtime.GOOS == "windows" {
+		pythonPath = "python"
+	}
+
+	_, err := looker.LookPath(pythonPath)
+	poetry, perr := looker.LookPath("poetry")
+
+	if err != nil {
+		fmt.Print(constants.FAIL_BACKGROUND.Render("ERROR"))
+		fmt.Println(constants.FAIL_FOREGROUND.Render(" python is not installed"))
+	} else if perr != nil {
+		fmt.Print(constants.FAIL_BACKGROUND.Render("ERROR"))
+		fmt.Println(constants.FAIL_FOREGROUND.Render(" poetry is not installed"))
+	} else {
+		dockerFileContent := templates.Content("poetry.dockerfile", "dockerfiles", botName)
+
+		mainFile := os.WriteFile(filepath.Join(botName, "src", "main.py"), []byte(python.MainPyContent()), 0644)
+		pyprojectFile := os.WriteFile(filepath.Join(botName, "pyproject.toml"), []byte(PyProjectContent(botName)), 0644)
+		dockerFile := os.WriteFile(filepath.Join(botName, "Dockerfile"), []byte(dockerFileContent), 0644)
+		resourcesFile := os.WriteFile(filepath.Join(botName, "resources.md"), []byte(python.Resources()), 0644)
+
+		if mainFile != nil {
+			log.Fatal(mainFile)
+		}
+
+		if pyprojectFile != nil {
+			log.Fatal(pyprojectFile)
+		}
+
+		if dockerFile != nil {
+			log.Fatal(dockerFile)
+		}
+
+		if resourcesFile != nil {
+			log.Fatal(resourcesFile)
+		}
+
+		poetryAdd := poetry + " add slack-bolt botway.py pyyaml"
+
+		cmd := exec.Command("bash", "-c", poetryAdd)
+
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command("powershell.exe", poetryAdd)
+		}
+
+		cmd.Dir = botName
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err = cmd.Run()
+
+		if err != nil {
+			log.Printf("error: %v\n", err)
+		}
+
+		templates.CheckProject(botName, "discord")
+	}
+}
