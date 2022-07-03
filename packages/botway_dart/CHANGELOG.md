047618c30099ac92b3ecba3c7533bd76421ea389
abdfnx <abdfnabd@gmail.com>
2022-07-03 20:51:03 +0300

    bump `botway_dart` to `v0.1.1`

--- packages/botway_dart/CHANGELOG.md
+++ packages/botway_dart/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.1.1
+
+- Fix some issues
+
 ## 0.1.0
 
 - Initial version.
---
+++ packages/botway_dart/examples/example.dart
@@ -0,0 +1,7 @@
+import "package:botway_dart/botway_dart.dart";
+
+void main() {
+  var bot = Botway();
+
+  print(bot.Get_Token());
+}
