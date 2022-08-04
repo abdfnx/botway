a73a33e77ce7f0099e162361dc02a31473bbed8a
Abdfn <64256993+abdfnx@users.noreply.github.com>
2022-08-04 19:55:51 +0000

    format `botway.json`, add `botway-c-config.json` generator

--- internal/pipes/token/discord/token.go
+++ internal/pipes/token/discord/token.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/abdfnx/botway/constants"
 	token_shared "github.com/abdfnx/botway/internal/pipes/token"
+	"github.com/abdfnx/botway/tools"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/tidwall/sjson"
@@ -45,6 +46,9 @@ func (m model) AddToken() {
 
 	fmt.Print(constants.SUCCESS_BACKGROUND.Render("SUCCESS"))
 	fmt.Println(constants.SUCCESS_FOREGROUND.Render(" " + m.botName + " Discord tokens're added successfully"))
+
+	tools.GenerateCConfig(m.inputs[0].Value())
+
 	fmt.Println(constants.SUCCESS_FOREGROUND.Render("You can add the guild ids of your discord server via the command") + token_shared.BoldStyle.Render(" "+"botway tokens add-guilds") + " 📁")
 }
 
---
+++ tools/generate-c-config.go
@@ -0,0 +1,52 @@
+package tools
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/abdfnx/botway/constants"
+	"github.com/spf13/viper"
+)
+
+func GenerateCConfig(token string) {
+	viper.AddConfigPath(constants.BotwayDirPath())
+	viper.SetConfigName("botway-c-config")
+	viper.SetConfigType("json")
+
+	viper.SetDefault("logging.level", "trace")
+	viper.SetDefault("logging.filename", "bot.log")
+	viper.SetDefault("logging.quiet", false)
+	viper.SetDefault("logging.overwrite", true)
+	viper.SetDefault("logging.use_color", true)
+	viper.SetDefault("logging.http.enable", true)
+	viper.SetDefault("logging.http.filename", "http.log")
+	viper.SetDefault("logging.disable_modules", []string{"WEBSOCKETS", "USER_AGENT"})
+	viper.SetDefault("discord.token", token)
+	viper.SetDefault("discord.default_prefix.enable", true)
+
+	if err := viper.SafeWriteConfig(); err != nil {
+		if os.IsNotExist(err) {
+			err = viper.WriteConfig()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Fatal(err)
+		}
+	}
+
+	if _, err := os.Stat(constants.BotwayConfigFile); err == nil {
+		fmt.Print(constants.SUCCESS_BACKGROUND.Render("SUCCESS"))
+		fmt.Println(constants.SUCCESS_FOREGROUND.Render(" Botway C Config Initialization Successful"))
+	} else if errors.Is(err, os.ErrNotExist) {
+		fmt.Print(constants.FAIL_BACKGROUND.Render("ERROR"))
+		fmt.Println(constants.FAIL_FOREGROUND.Render(" Botway C Config Initialization Failed, try again"))
+	}
+}
