25754fc6be42e3f5b4b4ba94734014c6294c209a
abdfnx <abdfnabd@gmail.com>
2022-09-17 11:20:57 +0300

    update `botway.py` package

--- packages/botway.py/botway/__init__.py
+++ packages/botway.py/botway/__init__.py
@@ -7,7 +7,7 @@ Python client package for Botway.
 __author__ = 'abdfnx'
 __license__ = 'MIT'
 __copyright__ = 'Copyright (c) 2022-now Abdfn'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 import yaml
 import json
