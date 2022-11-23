0a4002f5d4a829df163d2d064d9beede41e0d6e7
Abdfn <abdfnabd@gmail.com>
2022-11-23 21:05:20 +0300

    update botway cdn url

--- installer/unix/install.sh
+++ installer/unix/install.sh
@@ -4,11 +4,11 @@ echo Installing Botway installer...
 
 # Download the installer
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-    wget https://cdn-botway.up.railway.app/installers/installer-macos -O bw-installer
+    wget https://botway-cdn.onrender.com/installers/installer-macos -O bw-installer
 fi
 
 if [[ "$OSTYPE" =~ ^linux ]]; then
-    wget https://cdn-botway.up.railway.app/installers/installer-linux -O bw-installer
+    wget https://botway-cdn.onrender.com/installers/installer-linux -O bw-installer
 fi
 
 # Make it an executable
