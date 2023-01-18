5bd3d1346af186845c0aa31e2e0356941a765e58
abdfnx <abdfnabd@gmail.com>
2023-01-18 18:05:39 +0000

    stringify env vars in `./scripts/dot/main.go`

--- scripts/dot/main.go
+++ scripts/dot/main.go
@@ -6,9 +6,9 @@ import (
 )
 
 func main() {
-	fmt.Println("MONGO_URL=" + os.Getenv("MONGO_URL"))
-	fmt.Println("NEXT_PUBLIC_FULL=" + os.Getenv("NEXT_PUBLIC_FULL"))
-	fmt.Println("EMAIL_FROM=" + os.Getenv("EMAIL_FROM"))
-	fmt.Println("SENDGRID_API_KEY=" + os.Getenv("SENDGRID_API_KEY"))
-	fmt.Println("NEXT_PUBLIC_BW_SECRET_KEY=" + os.Getenv("NEXT_PUBLIC_BW_SECRET_KEY"))
+	fmt.Println(`MONGO_URL="` + os.Getenv("MONGO_URL") + `"`)
+	fmt.Println(`NEXT_PUBLIC_FULL=` + os.Getenv("NEXT_PUBLIC_FULL"))
+	fmt.Println(`EMAIL_FROM="` + os.Getenv("EMAIL_FROM") + `"`)
+	fmt.Println(`SENDGRID_API_KEY="` + os.Getenv("SENDGRID_API_KEY") + `"`)
+	fmt.Println(`NEXT_PUBLIC_BW_SECRET_KEY="` + os.Getenv("NEXT_PUBLIC_BW_SECRET_KEY") + `"`)
 }
