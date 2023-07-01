408bff74893105b2c8757d853efa71d83c951b09
Abdfnx Mhd <abdfnabd@gmail.com>
2023-07-01 19:48:09 +0000

    botway(core): start implementing AI for botway

--- core/app/api/ai/generator/route.ts
+++ core/app/api/ai/generator/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 import { fetcher } from "@/tools/fetch";
+import { jwtDecrypt } from "jose";
+import { BW_SECRET_KEY } from "@/tools/tokens";
 import createClient from "@/supabase/server";
+import { Octokit } from "octokit";
 
 export const revalidate = 0;
 
@@ -17,6 +20,22 @@ export async function POST(request: Request) {
   if (userError) {
     return NextResponse.json({ error: userError });
   }
+
+  const { payload: githubApiToken } = await jwtDecrypt(
+    user?.user_metadata["githubApiToken"],
+    BW_SECRET_KEY
+  );
+
+  const { payload: railwayApiToken } = await jwtDecrypt(
+    user?.user_metadata["railwayApiToken"],
+    BW_SECRET_KEY
+  );
+
+  const octokit = new Octokit({
+    auth: githubApiToken.data,
+  });
+
+  const ghu = await (await octokit.request("GET /user", {})).data;
 
   return NextResponse.json({ message: "Success" });
 }
